88f6c0a646ec7b182ea948cbe1f2c2cea5c02465
Gabriel Sbruzzi <132472378+GabrielSbruzzi@users.noreply.github.com>
2024-11-25 11:41:33 -0300

    Create VerificarIdade.java

---
+++ Exercícios Básicos/Exercicio 1/VerificarIdade.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class VerificarIdade {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade >= 18) {
+            System.out.print("Você é maior de idade.");
+        } else {
+            System.out.print("Você é menor de idade.");
+        }
+
+        scanner.close();
+    }
+}
