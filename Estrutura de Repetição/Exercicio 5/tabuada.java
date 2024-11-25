404fb863723c1e23951b51e2fa194d9afd46902a
Gabriel Sbruzzi <132472378+GabrielSbruzzi@users.noreply.github.com>
2024-11-25 11:30:38 -0300

    Create tabuada.java

---
+++ Estrutura de Repetição/Exercicio 5/tabuada.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Tabuada {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para ver a tabuada: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada de " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.printf("%d x %d = %d\n", numero, i, resultado);
+        }
+
+        scanner.close();
+    }
+}
